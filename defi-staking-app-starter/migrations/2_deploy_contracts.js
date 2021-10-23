7b96611496250ba553798786ba5c505c160278be
Alejandro Oviedo <alexoviedo999@gmail.com>
2021-10-22 19:17:19 -0500

    added rewards functionality and added it to deployment functionality

--- defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -1,5 +1,23 @@
 const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
+    //Deploy Contracts
     await deployer.deploy(Tether);
+    const tether = await Tether.deployed();
+
+    //Deploy RWD Contract
+    await deployer.deploy(RWD);
+    const rwd = await RWD.deployed();
+
+    //Deploy DecentralBank
+    await deployer.deploy(DecentralBank, rwd.address, tether.address);
+    const decentralBank = await DecentralBank.deployed();
+
+    //Transfer all RWD tokens to Decentral bank
+    await rwd.transfer(decentralBank.address, '1000000000000000000000000');
+
+    //Distritbure 100 Tether tokens to investor
+    await tether.transfer(accounts[1], '1000000000000000000');
 };
